9c377fb757f855a1cdd22cf99d5ee22d1e8a3a0a
Erik Hjalmarsson <erik.hjalmarsson@soget.com>
2024-09-17 17:05:32 +0200

    fake test

--- ProductCatalogAPITests/UnitTest1.cs
+++ ProductCatalogAPITests/UnitTest1.cs
@@ -15,6 +15,7 @@ namespace ProductCatalogAPITests
             //var function = new Function1(new NullLoggerFactory());
             //var request = new DefaultHttpRequestData(null);
             var fakeStorageKey = "DefaultEndpointsProtocol=https;AccountName=productcatalogstorage;AccountKey=QWERTYUIOPASDFGHJKLZXCVBNM1234567890==;EndpointSuffix=core.windows.net";
+            var myStorageConnection = "BlobEndpoint=https://storfabricpoc789.blob.core.windows.net/;QueueEndpoint=https://storfabricpoc789.queue.core.windows.net/;FileEndpoint=https://storfabricpoc789.file.core.windows.net/;TableEndpoint=https://storfabricpoc789.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=b&srt=o&sp=r&se=2024-09-17T15:14:01Z&st=2024-09-17T15:04:01Z&sip=155.4.33.77&spr=https&sig=H6zI6wCybmx3%2Be%2FyyfqWn0yLzRVnSMGkvJWmb5wfXTo%3D";
 
             //// Act
             //var response = function.Run(request);
